7742c62e1cb30941a9160523576ffc35c7d7defb
Felipe Vallina <felipeval@Felipes-MacBook-Pro.local>
2018-11-07 09:52:26 -0600

    todo GET

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ ES6 destructuring
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/Todo');
 const {User} = require('./models/User');
+const {ObjectID} = require('mongodb')
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -31,6 +32,24 @@ app.get('/todos', (req, res) => {
     }, (e)=> {
         res.status(400).send(e)
     })
+})
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send()
+            
+        }
+        res.send({todo})
+    }).catch((e) =>{
+        res.status(400).send()
+    })
+}, (e)=> {
+    res.status(404).send()
 })
 
 app.listen(3000, ()=>{
